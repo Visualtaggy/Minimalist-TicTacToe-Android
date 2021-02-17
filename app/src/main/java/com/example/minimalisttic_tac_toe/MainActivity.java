7d1415559b69618649d1d828e0a94c926214fc69
VisualTaggy <visualtaggy@gmail.com>
2021-02-18 00:53:50 +0530

    added functionality: place circle or cross

--- app/src/main/java/com/example/minimalisttic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/minimalisttic_tac_toe/MainActivity.java
@@ -3,8 +3,29 @@ package com.example.minimalisttic_tac_toe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+
+    public void fadeIn(View view){
+        //increasing counter to keep a track of the game
+        counter++;
+        if(counter % 2 == 0){
+            ImageView circle = (ImageView) view;
+            circle.setImageResource(R.drawable.circle);
+
+
+        }
+        else {
+            ImageView cross = (ImageView) view;
+            cross.setImageResource(R.drawable.cross);
+        }
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
